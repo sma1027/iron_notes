0931c4a5d66f08af90663d7c81b570e059d015b2
sofia ma <sofia.ma@gmail.com>
2015-01-03 19:02:52 -0500

    fixed

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,10 +6,55 @@ IronNotes.Router.map(function(){
   this.resource('notes', function(){
     this.resource('note', {path: "/:note_id"})
   });
-})
+});
 
 IronNotes.NotesRoute = Ember.Route.extend({
   model: function(){
     return this.store.find('note');
   }
-})
+});
+
+IronNotes.NoteController = Ember.ObjectController.extend({
+  editing: false,
+  actions: {
+    startEdit: function(){
+      this.set('editing', true)
+    },
+    saveNote: function(){
+      this.get('model');
+      var controller = this;
+      note.save().then(function(){
+        controller.set('editing', false);
+      });
+    }
+  }
+});
+
+IronNotes.NotesIndexController = Ember.ArrayController.extend({
+  newTitle: '',
+  newBody: '',
+  newPriorityLevel: '',
+
+  actions: {
+    createNote: function(){
+      var title = this.get('newTitle');
+      var body = this.get('newBody');
+      var priorityLevel = this.get('newPriorityLevel');
+
+      var note = this.store.createRecord('note', {
+        title: title,
+        body: body,
+        priorityLevel: priorityLevel
+      });
+
+      var controller = this;
+
+      note.save().then(function(model){
+        controller.get('newTitle', '');
+        controller.get('newBody', '');
+        controller.get('newPriorityLevel', '');
+        controller.transitionToRoute('note', model);
+      });
+    }
+  }
+});
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,17 @@ class NotesController < ApplicationController
   def index
     render json: Note.all
   end
+
+  def create
+    safe_note = params.require(:note).permit(:body, :title, :priority_level)
+    note = Note.create(safe_note)
+    render json: note
+  end
+
+  def update
+    safe_note = params.require(:note).permit(:body, :title, :priority_level)
+    note = Note.find(params[:id])
+    note.update(safe_note)
+    render json: note
+  end
 end
